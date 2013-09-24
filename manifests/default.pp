413336c265e5f74f9f18dd2867950d517c294d17
Matthew Tardiff <mattrix@gmail.com>
2013-09-24 16:44:11 +0100

    Puppet now ensures that LXC has an AppArmor profile set to complain instead of enforce. This allows privileged Docker containers to run without AppArmor getting in the way.

--- manifests/default.pp
+++ manifests/default.pp
@@ -13,3 +13,20 @@ package { 'socat':
 package { 'tmux':
     ensure => installed,
 }
+
+package { 'apparmor-utils':
+    ensure => installed,
+}
+
+exec { 'lxc-complain':
+    path    => ['/usr/sbin'],
+    command => 'aa-complain /etc/apparmor.d/lxc/lxc-default /usr/bin/lxc-start',
+    require => Package['apparmor-utils'],
+    notify  => Exec['reload-apparmor'],
+}
+
+exec { 'reload-apparmor':
+    path        => ['/usr/sbin', '/usr/local/sbin', '/usr/bin', '/usr/local/bin', '/bin'],
+    command     => 'service apparmor reload',
+    refreshonly => true,
+}
