b2556d7e1478322cb4e371a520f3017aef1ca69d
Matthew Tardiff <mattrix@gmail.com>
2013-09-27 22:28:35 +0100

    Now mostly puppetized, although some cleanup would be nice

--- manifests/initial_run.pp
+++ manifests/initial_run.pp
@@ -1,21 +1,27 @@
 
 ### High-level relationships
 
-# Update apt before installing any packages.
-Exec['apt-get update'] -> Package <| |>
+# Update apt before installing any packages, before rebooting
+Exec['apt-get update'] -> Package <| |> -> Exec['restart']
+
 
 ### Update apt
 
-# TODO: necessary?
-#exec { 'add-docker-gpg-key':
-#    path    => ['/usr/bin', '/bin'],
-#    command => "wget -q -O - https://get.docker.io/gpg | apt-key add -",
-#    unless  => "apt-key list | grep -s -q Docker",
-#}
+exec { 'add-docker-gpg-key':
+    path    => ['/usr/bin', '/bin'],
+    command => "wget -q -O - https://get.docker.io/gpg | apt-key add -",
+    unless  => "apt-key list | grep -s -q Docker",
+}
+
+file { 'docker-apt-list':
+    ensure  => file,
+    path    => "/etc/apt/sources.list.d/docker.list",
+    content => "deb http://get.docker.io/ubuntu docker main",
+}
 
 exec { 'apt-get update':
     path    => ['/usr/bin'],
-#    require => Exec['add-docker-gpg-key'],
+    require => [Exec['add-docker-gpg-key'], File['docker-apt-list']],
 }
 
 
@@ -23,26 +29,10 @@ exec { 'apt-get update':
 
 package { 'linux-image-generic-lts-raring':
     ensure => installed,
-} #~>
-#exec { 'restart':
-#    path        => ['/sbin'],
-#    command     => "shutdown -r now",
-#    refreshonly => true,
-#}
+}
 
 
-# TODO: install VBoxGuestAdditions:
-#        if ENV["VAGRANT_DEFAULT_PROVIDER"].nil? && ARGV.none? { |arg| arg.downcase.start_with?("--provider") }
-#            pkg_cmd << "apt-get install -q -y linux-headers-generic-lts-raring dkms; " \
-#                "echo 'Downloading VBox Guest Additions...'; " \
-#                "wget -q http://dlc.sun.com.edgesuite.net/virtualbox/4.2.18/VBoxGuestAdditions_4.2.18.iso; "
-#            # Prepare the VM to add guest additions after reboot
-#            pkg_cmd << "echo -e 'mount -o loop,ro /home/vagrant/VBoxGuestAdditions_4.2.18.iso /mnt\n" \
-#                "echo yes | /mnt/VBoxLinuxAdditions.run\numount /mnt\n" \
-#                "rm /root/guest_additions.sh; ' > /root/guest_additions.sh; " \
-#                "chmod 700 /root/guest_additions.sh; " \
-#                "sed -i -E 's#^exit 0#[ -x /root/guest_additions.sh ] \\&\\& /root/guest_additions.sh#' /etc/rc.local; "
-#        end
+### Install VirtualBox Guest Additions
 
 package { 'linux-headers-generic-lts-raring':
     ensure => installed,
@@ -53,22 +43,43 @@ package { 'dkms':
 }
 
 $vbox_version = "4.2.18"
+$vbox_guest_additions_iso = "VBoxGuestAdditions_${vbox_version}.iso"
+$vbox_guest_additions_src = "http://dlc.sun.com.edgesuite.net/virtualbox/${vbox_version}/${vbox_guest_additions_iso}"
+$vbox_guest_additions_dst = "/root/${vbox_guest_additions_iso}"
 
-exec { 'dl-vbox-guest-additions':
-    path    => ['/usr/bin'],
-    command => "wget -o /root/VBoxGuestAdditions_${vbox_version}.iso http://dlc.sun.com.edgesuite.net/virtualbox/${vbox_version}/VBoxGuestAdditions_${vbox_version}.iso",
+#exec { 'dl-vbox-guest-additions':
+#    path    => ['/usr/bin'],
+#    command => "wget -O ${vbox_guest_additions_dst} ${vbox_guest_additions_src}",
+#    creates => "${vbox_guest_additions_dst}",
+#    timeout => 1800,
+#}
+
+file { 'dl-vbox-guest-additions':
+    ensure => file,
+    path   => "${vbox_guest_additions_dst}",
+    source => "/vagrant/${vbox_guest_additions_iso}",
 }
 
 file { '/root/guest_additions.sh':
     ensure  => file,
     mode    => '700',
     content => "
-        mount -o loop,ro /home/vagrant/VBoxGuestAdditions_${vbox_version}.iso /mnt
+        mount -o loop,ro ${vbox_guest_additions_dst} /mnt
         echo yes | /mnt/VBoxLinuxAdditions.run
         umount /mnt
-        rm /root/guest_additions.sh",
+        mv /root/guest_additions.sh /root/guest_additions.sh.ran",
 }
 
-exec { '//':
+exec { 'queue-vbox-guest-additions':
+    path    => ['/bin'],
     command => "sed -i -E 's#^exit 0#[ -x /root/guest_additions.sh ] \\&\\& /root/guest_additions.sh#' /etc/rc.local",
+} ~>
+exec { 'restart':
+    path        => ['/sbin'],
+    command     => "shutdown -r now",
+    refreshonly => true,
+    require     => [
+        File['dl-vbox-guest-additions'],
+        File['/root/guest_additions.sh'],
+        Exec['queue-vbox-guest-additions']]
 }
