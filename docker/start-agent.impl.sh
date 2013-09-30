51efe49e0656d480769908967fcf30be5feaa6b7
Matthew Tardiff <mattrix@gmail.com>
2013-09-30 07:13:04 -0500

    TeamCity Server and Agent now use volumes for persistent storage. They're bound to a subfolder of /vagrant so that they're available even after the host is destroyed.

--- docker/start-agent.impl.sh
+++ docker/start-agent.impl.sh
@@ -20,11 +20,32 @@ docker__build_if_necessary \
     mattrix/teamcity-agent \
     teamcity-agent/3-teamcity-agent/Dockerfile || exit 1
 
+readonly HOST_BASE_DIR="/vagrant/.data/teamcity-agent"
+readonly HOST_SHARE_DIR="/vagrant/.data/teamcity-shared"
+
+readonly CONT_DATA_DIR="/root/.BuildServer"
+readonly HOST_DATA_DIR="${HOST_BASE_DIR}${CONT_DATA_DIR}"
+
+readonly CONT_WORK_DIR="/usr/local/TeamCity/buildAgent/work"
+readonly HOST_WORK_DIR="${HOST_BASE_DIR}${CONT_WORK_DIR}"
+
+readonly CONT_SSH_DIR="/root/.ssh"
+readonly HOST_SSH_DIR="${HOST_SHARE_DIR}${CONT_SSH_DIR}"
+
+# Create non-existant directories on the host
+[ -d "$HOST_DATA_DIR" ] || mkdir -p "$HOST_DATA_DIR"
+[ -d "$HOST_WORK_DIR" ] || mkdir -p "$HOST_WORK_DIR"
+[ -d "$HOST_SSH_DIR"  ] || mkdir -p "$HOST_SSH_DIR"
+
 docker_host_ip=$1
 docker_host_tc_server=$2
 docker_host_tc_agent=$3
 
-docker run -d -privileged mattrix/teamcity-agent \
-    "${docker_host_ip}" \
-    "${docker_host_tc_server}" \
-    "${docker_host_tc_agent}"
+docker run -d -privileged \
+    -v $HOST_DATA_DIR:$CONT_DATA_DIR \
+    -v $HOST_WORK_DIR:$CONT_WORK_DIR \
+    -v $HOST_SSH_DIR:$CONT_SSH_DIR \
+    mattrix/teamcity-agent \
+        "${docker_host_ip}" \
+        "${docker_host_tc_server}" \
+        "${docker_host_tc_agent}"
--- docker/start-server.impl.sh
+++ docker/start-server.impl.sh
@@ -14,4 +14,20 @@ docker__build_if_necessary \
     mattrix/teamcity-server \
     teamcity-server/2-teamcity-server/Dockerfile || exit 1
 
-docker run -d mattrix/teamcity-server
+readonly HOST_BASE_DIR="/vagrant/.data/teamcity-server"
+readonly HOST_SHARE_DIR="/vagrant/.data/teamcity-shared"
+
+readonly CONT_DATA_DIR="/root/.BuildServer"
+readonly HOST_DATA_DIR="${HOST_BASE_DIR}${CONT_DATA_DIR}"
+
+readonly CONT_SSH_DIR="/root/.ssh"
+readonly HOST_SSH_DIR="${HOST_SHARE_DIR}${CONT_SSH_DIR}"
+
+# Create non-existant directories on the host
+[ -d "$HOST_DATA_DIR" ] || mkdir -p "$HOST_DATA_DIR"
+[ -d "$HOST_SSH_DIR"  ] || mkdir -p "$HOST_SSH_DIR"
+
+docker run -d \
+    -v $HOST_DATA_DIR:$CONT_DATA_DIR \
+    -v $HOST_SSH_DIR:$CONT_SSH_DIR \
+    mattrix/teamcity-server
