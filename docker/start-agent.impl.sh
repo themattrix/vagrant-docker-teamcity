7879dadb80ac5356f80319ea7a10b14ccd41bf11
Matthew Tardiff <mattrix@gmail.com>
2013-10-09 17:05:27 -0500

    The docker data directory on the TeamCity Agent is now mounted to the host so that docker data persists between runs of the agent

--- docker/start-agent.impl.sh
+++ docker/start-agent.impl.sh
@@ -22,24 +22,27 @@ docker__build_if_necessary \
 
 readonly HOST_BASE_DIR="/vagrant/.data/teamcity-agent"
 readonly HOST_SHARE_DIR="/vagrant/.data/teamcity-shared"
+readonly HOST_LOCAL_DIR="/root/.data"
 
 readonly CONT_DATA_DIR="/root/.BuildServer"
 readonly HOST_DATA_DIR="${HOST_BASE_DIR}${CONT_DATA_DIR}"
+[ -d "$HOST_DATA_DIR" ] || mkdir -p "$HOST_DATA_DIR"
 
 readonly CONT_WORK_DIR="/usr/local/TeamCity/buildAgent/work"
 readonly HOST_WORK_DIR="${HOST_BASE_DIR}${CONT_WORK_DIR}"
+[ -d "$HOST_WORK_DIR" ] || mkdir -p "$HOST_WORK_DIR"
 
 readonly CONT_CONF_DIR="/usr/local/TeamCity/buildAgent/conf/persist"
 readonly HOST_CONF_DIR="${HOST_BASE_DIR}${CONT_CONF_DIR}"
+[ -d "$HOST_CONF_DIR" ] || mkdir -p "$HOST_CONF_DIR"
 
 readonly CONT_SSH_DIR="/root/.ssh"
 readonly HOST_SSH_DIR="${HOST_SHARE_DIR}${CONT_SSH_DIR}"
-
-# Create non-existant directories on the host
-[ -d "$HOST_DATA_DIR" ] || mkdir -p "$HOST_DATA_DIR"
-[ -d "$HOST_WORK_DIR" ] || mkdir -p "$HOST_WORK_DIR"
 [ -d "$HOST_SSH_DIR"  ] || mkdir -p "$HOST_SSH_DIR"
-[ -d "$HOST_CONF_DIR" ] || mkdir -p "$HOST_CONF_DIR"
+
+readonly CONT_DOCK_DIR="/var/lib/docker"
+readonly HOST_DOCK_DIR="${HOST_LOCAL_DIR}${CONT_DOCK_DIR}"
+[ -d "$HOST_DOCK_DIR" ] || mkdir -p "$HOST_DOCK_DIR"
 
 docker_host_ip=$1
 docker_host_tc_server=$2
@@ -50,6 +53,7 @@ docker run -d -privileged \
     -v $HOST_WORK_DIR:$CONT_WORK_DIR \
     -v $HOST_SSH_DIR:$CONT_SSH_DIR \
     -v $HOST_CONF_DIR:$CONT_CONF_DIR \
+    -v $HOST_DOCK_DIR:$CONT_DOCK_DIR \
     mattrix/teamcity-agent \
         "${docker_host_ip}" \
         "${docker_host_tc_server}" \
